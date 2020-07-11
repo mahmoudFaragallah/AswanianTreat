b01cfb9f8c541b4f1055fbe135228b8228d69160
Mahmoud Faragallah <mahmoudahmedfarajallah@outlook.com>
2020-07-11 02:28:26 +0200

    Add project files.

---
+++ wwwroot/js/index.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    console.log("hello mahmoud");
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+
+    // Anonymous function
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You Clicked on" + $(this).text());
+    });
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(200);
+    })
+});
